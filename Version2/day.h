d034ee93bd9de9737d069809b1989836e62ee369
Tointech <leanhthunk@gmail.com>
2021-12-20 15:37:54 +0700

    update commit

---
+++ Version2/day.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <ctime>
+
+struct Today {
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    int todayyear = 1900 + ltm->tm_year;
+    int todaymonth = 1 + ltm->tm_mon;
+    int todayday = ltm->tm_mday;
+};
